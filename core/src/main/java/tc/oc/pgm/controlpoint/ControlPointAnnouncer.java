f1fba6f8515166050f2b3644f18ca7c979fdd462
Jason Ho <jason@jasonho.tk>
2020-06-06 16:13:50 -0700

    Make KOTH chat messages stand out from other messages.
    Signed-off-by: Jason Ho <jason@jasonho.tk>

--- core/src/main/java/tc/oc/pgm/controlpoint/ControlPointAnnouncer.java
+++ core/src/main/java/tc/oc/pgm/controlpoint/ControlPointAnnouncer.java
@@ -1,5 +1,7 @@
 package tc.oc.pgm.controlpoint;
 
+import net.kyori.text.Component;
+import net.kyori.text.ComponentBuilder;
 import net.kyori.text.TextComponent;
 import net.kyori.text.format.TextColor;
 import org.bukkit.event.EventHandler;
@@ -7,10 +9,14 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import tc.oc.pgm.api.match.Match;
 import tc.oc.pgm.controlpoint.events.ControllerChangeEvent;
-import tc.oc.pgm.util.text.TextFormatter;
+import tc.oc.pgm.util.named.NameStyle;
 
 public class ControlPointAnnouncer implements Listener {
   private final Match match;
+
+  private static final Component NEWLINE = TextComponent.newline();
+  private static final String SPACES = "    ";
+  private static final String ARROW_SYMBOL = "⇨";
 
   public ControlPointAnnouncer(Match match) {
     this.match = match;
@@ -19,25 +25,31 @@ public class ControlPointAnnouncer implements Listener {
   @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
   public void onOwnerChange(ControllerChangeEvent event) {
     if (event.getControlPoint().isVisible()) {
-
-      if (event.getOldController() != null && event.getNewController() == null) {
-        this.match.sendMessage(
-            TextComponent.builder()
-                .append(event.getOldController().getName())
-                .append(" lost ", TextColor.GRAY)
-                .append(event.getControlPoint().getName(), TextColor.WHITE)
-                .build());
-
-      } else if (event.getNewController() != null) {
-        this.match.sendMessage(
-            TextComponent.builder()
-                .append(event.getNewController().getName())
-                .append(" captured ", TextColor.GRAY)
-                .append(
-                    event.getControlPoint().getName(),
-                    TextFormatter.convert(event.getNewController().getColor()))
-                .build());
+      ComponentBuilder message = TextComponent.builder(SPACES + ARROW_SYMBOL + SPACES);
+      if (event.getOldController() != null && event.getNewController() != null) {
+        message
+            .append(event.getOldController().getName(NameStyle.COLOR))
+            .append(" lost ", TextColor.GRAY)
+            .append(event.getControlPoint().getName())
+            .append(" to ", TextColor.GRAY)
+            .append(event.getNewController().getName(NameStyle.COLOR));
+      } else if (event.getOldController() != null && event.getNewController() == null) {
+        message
+            .append(event.getOldController().getName(NameStyle.COLOR))
+            .append(" lost ", TextColor.GRAY)
+            .append(event.getControlPoint().getName());
+      } else if (event.getOldController() == null && event.getNewController() != null) {
+        message
+            .append(event.getNewController().getName(NameStyle.COLOR))
+            .append(" captured ", TextColor.GRAY)
+            .append(event.getControlPoint().getName());
+      } else if (event.getOldController() == null && event.getNewController() == null) {
+        message.append(event.getControlPoint().getName()).append(" was lost", TextColor.GRAY);
       }
+      // separate it from other chat messages
+      this.match.sendMessage(NEWLINE);
+      this.match.sendMessage(message.build());
+      this.match.sendMessage(NEWLINE);
     }
   }
 }
